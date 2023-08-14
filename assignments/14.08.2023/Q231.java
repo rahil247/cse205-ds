d2a123940abb66ebe2052cd9c0a7a00eb8d69f8a
Rahil Patel <rahilpatel2474@gmail.com>
2023-08-14 14:13:41 +0530

    commit

---
+++ assignments/14.08.2023/Q231.java
@@ -0,0 +1,13 @@
+public class Q231 {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1){
+            return true;
+        }
+
+        if(n==0){
+            return false;
+        }
+
+        return n%2==0 && isPowerOfTwo(n/2);
+    }
+}
