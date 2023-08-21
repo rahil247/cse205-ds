321e3a54331d619a156c0754fd4a44816064ad1c
Rahil Patel <rahilpatel2474@gmail.com>
2023-08-21 13:50:44 +0530

    commit

--- assignments/14.08.2023/Q78.java
+++ assignments/14.08.2023/Q78.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class Q78 {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList();
-        subsets(0,nums, new ArrayList(),result);
+        List<List<Integer>> result = new ArrayList<>();
+        subsets(0,nums, new ArrayList<>(),result);
         return result;
     }
 
     void subsets(int idx, int[] nums, List<Integer> curr,  List<List<Integer>> result){
-        result.add(new ArrayList(curr)); //empty set initialised
+        result.add(new ArrayList<>(curr)); //empty set initialised
 
         for (int i = idx; i < nums.length; i++) {
             curr.add(nums[i]);
