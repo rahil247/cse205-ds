fc50965c580c0a98af9d557748146614d6f7b203
Rahil Patel <rahilpatel2474@gmail.com>
2023-08-29 18:10:51 +0530

    commit

--- assignments/28.08.2023/Q121.java
+++ assignments/28.08.2023/Q121.java
@@ -1,3 +1,18 @@
 public class Q121 {
-    
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        
+        int minPrice = prices[0]; 
+        int maxProfit = 0;        
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;  
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);  
+            }
+        }
+        return maxProfit;
+    }
 }
