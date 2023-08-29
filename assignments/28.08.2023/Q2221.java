0792c1819c64918696837b437a49e096ccc0067d
Rahil Patel <rahilpatel2474@gmail.com>
2023-08-29 18:41:35 +0530

    commit

--- assignments/28.08.2023/Q2221.java
+++ assignments/28.08.2023/Q2221.java
@@ -1,3 +1,12 @@
 public class Q2221 {
-    
+    public int triangularSum(int[] nums) {
+        while (nums.length > 1) {
+            int[] newNums = new int[nums.length - 1];
+            for (int i = 0; i < nums.length - 1; i++) {
+                newNums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            nums = newNums;
+        }
+        return nums[0];
+    }
 }
