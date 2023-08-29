4e215ec690d1fb8d32c744318ad0ec4f6b70e3dc
Rahil Patel <rahilpatel2474@gmail.com>
2023-08-29 18:19:37 +0530

    commit

--- assignments/28.08.2023/Q118.java
+++ assignments/28.08.2023/Q118.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q118{
-    
+     public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            int num = 1;
+            for (int j = 0; j <= i; j++) {
+                row.add(num);
+                num = num * (i - j) / (j + 1);
+            }
+            triangle.add(row);
+        }
+        
+        return triangle;
+    }
 }
